9ce9ffa5693b8838a7f5785c5918370cc3b8f49c
Gavan1312 <gavadnac@gmail.com>
2025-01-18 20:46:49 +0530

    Upload Project

---
+++ Grabber/config.py
@@ -0,0 +1,14 @@
+import os
+
+OWNER_ID = 880926547
+GROUP_ID = "-1002272695266"
+TOKEN = os.getenv("BOT_TOKEN")
+api_id = os.getenv("DEV_API_ID")
+api_hash = os.getenv("DEV_API_HASH")
+PHOTO_URL = ["https://github.com/Gavan1312/Character-Grabber-Bot/blob/main/Images/bot-icon.jpg?raw=true%22%20data-hpc=%22true"]
+SUPPORT_CHAT = "Gomu_Gomu_Syndicate"
+UPDATE_CHAT = "Gomu_Gomu_Syndicate"
+BOT_USERNAME = "Character_Grabber_Game_bot"
+CHARA_CHANNEL_ID = -1002272695266
+MONGO_URL = "mongodb+srv://justsomeuserforsecurityreasons:4CPkvYgyti5bqw9Y@cluster0.bklyr.mongodb.net/"
+DATABASE_NAME = "Character_grabber"
