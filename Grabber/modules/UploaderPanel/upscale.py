b6a9e1de4a2b663a75a699d37688c5d6dcaf14d3
Gavan1312 <gavadnac@gmail.com>
2025-02-05 07:45:49 +0530

    upscale with id command added

--- Grabber/modules/UploaderPanel/upscale.py
+++ Grabber/modules/UploaderPanel/upscale.py
@@ -1,17 +1,21 @@
 from pyrogram import Client, filters
 import base64
 import aiohttp
+import os
 from Grabber.modules import app, uploader_filter
 
-@app.on_message(filters.command("upscale") & uploader_filter)
-async def upscale_image(client, message):
-    #3.7 for glitch
+def get_caption_with_id(message):
+    """Extracts the ID from the caption if present."""
+    if message.caption and "id - " in message.caption.lower():
+        return message.caption.split("id - ")[-1].strip()
+    return None
+
+async def upscale_image(client, message, keep_id=False):
     reply = message.reply_to_message
     if not reply or not reply.photo:
         return await message.reply("Reply to an image to upscale it.")
     
     progress = await message.reply("Upscaling your image, please wait...")
-    
     image = await client.download_media(reply.photo.file_id)
     
     with open(image, "rb") as f:
@@ -19,8 +23,27 @@ async def upscale_image(client, message):
     
     async with aiohttp.ClientSession() as s:
         async with s.post("https://lexica.qewertyy.dev/upscale", data={"image_data": encoded}) as r:
-            with open("upscaled_image.png", "wb") as out:
+            output_path = "upscaled_image.png"
+            with open(output_path, "wb") as out:
                 out.write(await r.read())
     
     await progress.delete()
-    await message.reply_photo("upscaled_image.png", caption=f"**Upscaled by @{client.me.username}**")
+    caption = ""
+    
+    if keep_id:
+        image_id = get_caption_with_id(reply)
+        if image_id:
+            caption = f"id - {image_id}"
+    else:
+        caption = f"**Upscaled by @{client.me.username}**"
+    
+    await message.reply_photo(output_path, caption=caption if caption else None)
+    os.remove(output_path)  # Clean up the file
+
+@app.on_message(filters.command("upscale") & uploader_filter)
+async def upscale_command(client, message):
+    await upscale_image(client, message, keep_id=False)
+
+@app.on_message(filters.command("upscalewithid") & uploader_filter)
+async def upscale_with_id_command(client, message):
+    await upscale_image(client, message, keep_id=True)
