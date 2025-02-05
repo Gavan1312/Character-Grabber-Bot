d29e43dbcc9871b6bb8784e9560997f785342beb
Gavan1312 <gavadnac@gmail.com>
2025-02-05 08:50:08 +0530

    Upload with upscale & upscale with details

--- Grabber/modules/UploaderPanel/upscale.py
+++ Grabber/modules/UploaderPanel/upscale.py
@@ -10,15 +10,15 @@ def get_caption_with_id(message):
         return message.caption.split("id - ")[-1].strip()
     return None
 
-async def upscale_image(client, message, keep_id=False):
-    reply = message.reply_to_message
-    if not reply or not reply.photo:
-        return await message.reply("Reply to an image to upscale it.")
-    
-    progress = await message.reply("Upscaling your image, please wait...")
-    image = await client.download_media(reply.photo.file_id)
-    
-    with open(image, "rb") as f:
+def get_caption_with_details(message):
+    """Extracts name, anime, and rarity details from the caption."""
+    if message.caption:
+        return message.caption.strip()
+    return None
+
+async def process_upscale(image_path):
+    """Handles the actual image upscaling."""
+    with open(image_path, "rb") as f:
         encoded = base64.b64encode(f.read()).decode("utf-8")
     
     async with aiohttp.ClientSession() as s:
@@ -26,14 +26,28 @@ async def upscale_image(client, message, keep_id=False):
             output_path = "upscaled_image.png"
             with open(output_path, "wb") as out:
                 out.write(await r.read())
+    return output_path
+
+async def upscale_image(client, message, caption_type=None):
+    reply = message.reply_to_message
+    if not reply or not reply.photo:
+        return await message.reply("Reply to an image to upscale it.")
+    
+    progress = await message.reply("Upscaling your image, please wait...")
+    image = await client.download_media(reply.photo.file_id)
+    output_path = await process_upscale(image)
     
     await progress.delete()
     caption = ""
     
-    if keep_id:
+    if caption_type == "id":
         image_id = get_caption_with_id(reply)
         if image_id:
-            caption = f"id - {image_id}"
+            caption = f"ID - {image_id}"
+    elif caption_type == "details":
+        details = get_caption_with_details(reply)
+        if details:
+            caption = details
     else:
         caption = f"**Upscaled by @{client.me.username}**"
     
@@ -42,8 +56,12 @@ async def upscale_image(client, message, keep_id=False):
 
 @app.on_message(filters.command("upscale") & uploader_filter)
 async def upscale_command(client, message):
-    await upscale_image(client, message, keep_id=False)
+    await upscale_image(client, message)
 
 @app.on_message(filters.command("upscalewithid") & uploader_filter)
 async def upscale_with_id_command(client, message):
-    await upscale_image(client, message, keep_id=True)
+    await upscale_image(client, message, caption_type="id")
+
+@app.on_message(filters.command("upscale_with_details") & uploader_filter)
+async def upscale_with_details_command(client, message):
+    await upscale_image(client, message, caption_type="details")
--- Grabber/modules/nupload.py
+++ Grabber/modules/nupload.py
@@ -7,6 +7,7 @@ from . import uploader_filter, app, user_collection
 from Grabber import collection, db, CHARA_CHANNEL_ID, OWNER_ID
 from Grabber.modules.UploaderPanel.upload_catbox import upload_to_catbox
 from Grabber.modules.Settings.rarityMap import *
+from Grabber.modules.UploaderPanel.upscale import process_upscale
 
 async def get_next_sequence_number(sequence_name):
     sequence_collection = db.sequences
@@ -20,8 +21,52 @@ async def get_next_sequence_number(sequence_name):
         return 0
     return sequence_document['sequence_value']
 
-
 rarity_map = RARITY_TO_USE_NUMBER_MAPPING
+
+def parse_caption(caption_text):
+    caption = caption_text.strip().split("\n")
+    if len(caption) != 3:
+        return None, None, None
+    
+    try:
+        character_name = caption[0].split(" - ")[1].strip().title()
+        anime = caption[1].split(" - ")[1].strip().title()
+        rarity_str = caption[2].split(" - ")[1].strip()
+        rarity = rarity_map[int(rarity_str)]
+        return character_name, anime, rarity
+    except (KeyError, ValueError, IndexError):
+        return None, None, None
+
+async def upload_character(client, message, img_url, character_name, anime, rarity):
+    id = str(await get_next_sequence_number('character_id')).zfill(2)
+    # price = random.randint(60000, 80000)
+    user_link = f'[{message.from_user.first_name}](tg://user?id={message.from_user.id})'
+    
+    sent_message = await client.send_photo(
+        chat_id=CHARA_CHANNEL_ID,
+        photo=img_url,
+        caption=(
+            f'**Waifu Name:** {character_name}\n'
+            f'**Anime Name:** {anime}\n'
+            f'**Quality:** {rarity}\n'
+            # f'**Price: {price}\n'
+            f'**ID:** {id}\n'
+            f'**Added by:** {user_link}'
+        )
+    )
+
+    character = {
+        'img_url': img_url,
+        'name': character_name,
+        'anime': anime,
+        'rarity': rarity,
+        'price': price,
+        'id': id,
+        'message_id': sent_message.id
+    }
+
+    await collection.insert_one(character)
+    await message.reply_text('WAIFU ADDED....')
 
 @app.on_message(filters.command('upload') & uploader_filter)
 async def upload(client: Client, message: Message):
@@ -29,53 +74,33 @@ async def upload(client: Client, message: Message):
         await message.reply_text("Please reply to an image with the caption in the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number'")
         return
 
-    caption = message.reply_to_message.caption.strip().split("\n")
-    if len(caption) != 3:
-        await message.reply_text("Incorrect format. Please use the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number'")
-        return
-
-    try:
-        character_name = caption[0].split(" - ")[1].strip().title()
-        anime = caption[1].split(" - ")[1].strip().title()
-        rarity_str = caption[2].split(" - ")[1].strip()
-        rarity = rarity_map[int(rarity_str)]
-    except (KeyError, ValueError, IndexError):
-        await message.reply_text("Invalid format or rarity. Please use the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number' and ensure rarity is a number between 1 and 10.")
+    character_name, anime, rarity = parse_caption(message.reply_to_message.caption)
+    if not character_name:
+        await message.reply_text("Incorrect format or invalid rarity. Please use the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number'")
         return
 
     try:
         photo = await client.download_media(message.reply_to_message.photo)
-
         img_url = upload_to_catbox(photo)
+        await upload_character(client, message, img_url, character_name, anime, rarity)
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {str(e)}")
 
-        id = str(await get_next_sequence_number('character_id')).zfill(2)
-        price = random.randint(60000, 80000)
+@app.on_message(filters.command('upload_with_upscale') & uploader_filter)
+async def upload_with_upscale(client: Client, message: Message):
+    if not message.reply_to_message or not message.reply_to_message.photo:
+        await message.reply_text("Please reply to an image with the caption in the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number'")
+        return
 
-        sent_message = await client.send_photo(
-            chat_id=CHARA_CHANNEL_ID,
-            photo=img_url,
-            caption=(
-                f'Waifu Name: {character_name}\n'
-                f'Anime Name: {anime}\n'
-                f'Quality: {rarity}\n'
-                f'Price: {price}\n'
-                f'ID: {id}\n'
-                f'Added by {message.from_user.first_name}'
-            )
-        )
+    character_name, anime, rarity = parse_caption(message.reply_to_message.caption)
+    if not character_name:
+        await message.reply_text("Incorrect format or invalid rarity. Please use the format: 'Name - Name Here\nAnime - Anime Here\nRarity - Number'")
+        return
 
-        character = {
-            'img_url': img_url,
-            'name': character_name,
-            'anime': anime,
-            'rarity': rarity,
-            'price': price,
-            'id': id,
-            'message_id': sent_message.id
-        }
-
-        await collection.insert_one(character)
-        await message.reply_text('WAIFU ADDED....')
-
+    try:
+        photo = await client.download_media(message.reply_to_message.photo)
+        upscaled_photo = await process_upscale(photo)
+        img_url = upload_to_catbox(upscaled_photo)
+        await upload_character(client, message, img_url, character_name, anime, rarity)
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
